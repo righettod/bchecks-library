d5423de082123d761d88afe466e5c52b04fdb53f
Dominique RIGHETTO <righettod@users.noreply.github.com>
2023-10-06 08:30:38 +0200

    Update Test-Scripts.ps1

--- Test-Scripts.ps1
+++ Test-Scripts.ps1
@@ -1,5 +1,5 @@
 # Validate all BCheck scripts against the official validator provided.
-$JarUrl = "https://github.com/PortSwigger/BChecks/raw/main/BCheckChecker-1.2.jar"
+$JarUrl = "https://github.com/PortSwigger/BChecks/raw/main/BCheckChecker-1.3.jar"
 $Checker = ".\BCheckChecker.jar"
 if (-not(Test-Path -Path $Checker -PathType Leaf)) {
     Write-Host "Download the 'BCheckChecker.jar' file..."
